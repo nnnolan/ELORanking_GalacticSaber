8853ab786b853ba155c506c0e5ca65348680c54c
Nolan <pestanonolan@gmail.com>
2022-03-22 09:35:25 -0500

    base login

--- galaticsaber/login/models.py
+++ galaticsaber/login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.shortcuts import render
 
-# Create your models here.
+from django.contrib.auth.models import User, authenticate, login
+#models go here
+
+
+# user = User.objects.create_user( ## function needs to be filled from form
+#     username='test',                        
+#     email='test@test.com',
+#     password='test123'
+#     )
--- galaticsaber/login/views.py
+++ galaticsaber/login/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import login, authenticate
+
+from . import SignUpForm
 
 # Create your views here.
-def index(request):
-    return render(request, 'login/login.html')
+# def index(request):
+#     return render(request, 'login/login.html')
 
 def register(request):
-    return render(request, 'login/register.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login(request):
+    return render(request, 'login/login.html')
