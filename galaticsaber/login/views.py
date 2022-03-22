1b60bc83a2f8a56d6a4d44b3ce418b553a106cca
Nolan <pestanonolan@gmail.com>
2022-03-22 08:50:06 -0500

    set up urls

--- galaticsaber/login/views.py
+++ galaticsaber/login/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'login/index.html')
+
+def login(request):
+    return render(request, 'login/login.html')
+
+def register(request):
+    return render(request, 'login/register.html')
