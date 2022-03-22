a6e0a7fa6888f724276a0dec6a806918f38fc364
Nolan <pestanonolan@gmail.com>
2022-03-22 09:11:45 -0500

    admin creation. login page views

--- galaticsaber/login/views.py
+++ galaticsaber/login/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'login/index.html')
-
-def login(request):
     return render(request, 'login/login.html')
 
 def register(request):
