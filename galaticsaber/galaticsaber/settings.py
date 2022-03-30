ca330c49356746af2fbff4d727909dfe7874f3c7
PlantyDev <61752127+plantydev@users.noreply.github.com>
2022-03-30 09:58:09 -0500

    30.3.2022 Progress

--- galaticsaber/galaticsaber/settings.py
+++ galaticsaber/galaticsaber/settings.py
@@ -122,7 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-#test cookie
-SESSION_COOKIE_AGE = 60 * 60 * 24 * 180 # 180 days
--- galaticsaber/login/apps.py
+++ galaticsaber/login/apps.py
@@ -1,4 +1,6 @@
 from django.apps import AppConfig
+import django.contrib.admin
+import django.contrib.auth, django.contrib.contenttypes, django.contrib.messages, django.contrib.sessions 
 
 
 class AccountsConfig(AppConfig):
