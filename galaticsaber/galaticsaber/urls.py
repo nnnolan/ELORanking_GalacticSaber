2e0eede91a5d762e711565dfc13e2b1c3069b358
Nolan <pestanonolan@gmail.com>
2022-03-31 08:20:39 -0500

    main login base, needs functonality

--- galaticsaber/galaticsaber/urls.py
+++ galaticsaber/galaticsaber/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', include('login.urls')),
-
-    # path('login/', include('login.urls')),
+    path('admin/', admin.site.urls), 
+    path('',include('login.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
+#41:00 youtube video
--- galaticsaber/login/apps.py
+++ galaticsaber/login/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class AccountsConfig(AppConfig):
-    name = 'accounts'
+class MainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'login'
