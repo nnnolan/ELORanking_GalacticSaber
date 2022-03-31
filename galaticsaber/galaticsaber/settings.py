479c50fcddb0135ca1ecc55f98ee0b89f90ac7ca
PlantyDev <61752127+plantydev@users.noreply.github.com>
2022-03-31 08:17:36 -0500

    Added MTG Leaderboard View

--- galaticsaber/galaticsaber/settings.py
+++ galaticsaber/galaticsaber/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
