9839ccac415ffa8442cffafbc460aeb7f1cf924f
Nolan <pestanonolan@gmail.com>
2022-03-31 08:30:14 -0500

    cleanup for no errors

--- galaticsaber/login/views.py
+++ galaticsaber/login/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "main/home.html")
+
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+#     else:
+#         form = RegisterForm()
+    
+#     return render(request, 'registration/signup.html', {'form': form})
+        
