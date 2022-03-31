27970d99463d1ca61f4cce6f378a3b8ab957c31b
Nolan <pestanonolan@gmail.com>
2022-03-31 09:47:09 -0500

    admin and user functions init

--- galaticsaber/login/forms.py
+++ galaticsaber/login/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-
+   
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
