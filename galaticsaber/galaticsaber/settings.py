bb0c81738e60cf9e8c0cb5cf4f3b086095a6357b
Nolan <pestanonolan@gmail.com>
2022-04-13 08:33:07 -0500

    player views

--- galaticsaber/galaticsaber/settings.py
+++ galaticsaber/galaticsaber/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'login.apps.LoginConfig',
+    'login.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
