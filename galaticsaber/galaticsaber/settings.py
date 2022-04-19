027f06358fc115e662c0bf010d0e522dd5320bda
PlantyDev <61752127+plantydev@users.noreply.github.com>
2022-04-19 09:22:35 -0500

    Update settings.py

--- galaticsaber/galaticsaber/settings.py
+++ galaticsaber/galaticsaber/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,7 +110,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+<<<<<<< Updated upstream
 TIME_ZONE = 'UTC' # ??? 
+=======
+TIME_ZONE = 'CST'
+>>>>>>> Stashed changes
 
 USE_I18N = True
 
@@ -120,8 +125,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+<<<<<<< Updated upstream
 
 
+=======
+#------------------------------------------------------------------------------
+STATIC_ROOT = os.path.join(BASE_DIR, 'boot'),
+#------------------------------------------------------------------------------
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'boot'),
+]
+>>>>>>> Stashed changes
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
